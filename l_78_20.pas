c8bf0c4f3e96084fe14ff49adb06214718704963
Savely <112480298+savacn@users.noreply.github.com>
2023-11-27 12:51:25 +0300

    Add files via upload

---
+++ l_78_20.pas
@@ -0,0 +1,16 @@
+﻿program l_78_20;
+
+var
+  s: string;
+  i, j, l: integer;
+
+begin
+  writeln('Введите строку: ');
+  readln(s);
+  i:=1;
+  while i <= length(s) do
+    if (s[i] = ' ') and (s[i + 1] = ' ') then
+      delete(s, i, 1)
+    else inc(i);
+  writeln('Итог: ', s); 
+end.
---
+++ l_78_5.pas
@@ -0,0 +1,14 @@
+﻿program l_78_5;
+
+var
+  s: string;
+  i: integer;
+  
+begin
+  writeln('Введите строку:');
+  readln (s);
+  writeln ('Номера символов, совпадающих с последним: ');
+  for i:=1 to length(s)-1 do
+    if s[i] = s[length(s)] then
+      write (i, ' ');
+end.
